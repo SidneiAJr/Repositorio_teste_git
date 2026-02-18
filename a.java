b57a15e5cdf2ad5586a4528e869ff6772290a29c
SIDNEI ALBERTO KLEISCHMITT JUNIOR <03541674059@senacrs.edu.br>
2026-02-18 16:39:09 -0300

    Alteração

--- a.java
+++ a.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Fala dai | COE");
+        double a = 5.5;
+        double b = 5.5;
+        System.out.println("AAAA")
     }
 }
